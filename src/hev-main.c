6c20633e7a8008f35530da6e91fd639bed273af4
Heiher <r@hev.cc>
2018-11-01 20:19:09 +0800

    HevMain: Enable TCP port forwarding.

--- src/hev-main.c
+++ src/hev-main.c
@@ -20,6 +20,8 @@
 #include "hev-fsh-server.h"
 #include "hev-fsh-client-term-forward.h"
 #include "hev-fsh-client-term-connect.h"
+#include "hev-fsh-client-port-forward.h"
+#include "hev-fsh-client-port-listen.h"
 
 #define MAJOR_VERSION (1)
 #define MINOR_VERSION (4)
@@ -29,19 +31,21 @@ static void
 show_help (void)
 {
     fprintf (stderr, "\tServer: [-a ADDRESS] [-p PORT] [-l LOG]\n");
-    fprintf (stderr,
-             "\tClient Forward: -s ADDRESS [-p PORT] [-u USER] [-t TOKEN]\n");
-    fprintf (stderr, "\tClient Connect: -s ADDRESS [-p PORT] -c TOKEN\n");
+    fprintf (stderr, "\tClient Common: -s ADDRESS [-p PORT]\n");
+    fprintf (stderr, "\t\tTerminal Forward: [-u USER] [-t TOKEN]\n");
+    fprintf (stderr, "\t\tTerminal Connect: -c TOKEN\n");
+    fprintf (stderr, "\t\tTCP Port Forward: -o ADDRESS -x PORT [-t TOKEN]\n");
+    fprintf (stderr, "\t\tTCP Port Connect: -c TOKEN [-o ADDRESS] -x PORT\n");
     fprintf (stderr, "Version: %d.%d.%d\n", MAJOR_VERSION, MINOR_VERSION,
              MICRO_VERSION);
 }
 
 static void
-server_run (const char *listen_address, unsigned int port)
+server_run (const char *lis_addr, unsigned int port)
 {
     HevFshServer *server;
 
-    server = hev_fsh_server_new (listen_address, port);
+    server = hev_fsh_server_new (lis_addr, port);
     if (!server) {
         fprintf (stderr, "Create fsh server failed!\n");
         return;
@@ -55,14 +59,13 @@ server_run (const char *listen_address, unsigned int port)
 }
 
 static void
-client_term_forward_run (const char *server_address, unsigned int port,
+client_term_forward_run (const char *srv_addr, unsigned int port,
                          const char *user, const char *token)
 {
     for (;;) {
         HevFshClientTermForward *client;
 
-        client =
-            hev_fsh_client_term_forward_new (server_address, port, user, token);
+        client = hev_fsh_client_term_forward_new (srv_addr, port, user, token);
 
         hev_task_system_run ();
 
@@ -73,12 +76,46 @@ client_term_forward_run (const char *server_address, unsigned int port,
 }
 
 static void
-client_term_connect_run (const char *server_address, unsigned int port,
+client_term_connect_run (const char *srv_addr, unsigned int port,
                          const char *token)
 {
     HevFshClientTermConnect *client;
 
-    client = hev_fsh_client_term_connect_new (server_address, port, token);
+    client = hev_fsh_client_term_connect_new (srv_addr, port, token);
+
+    hev_task_system_run ();
+
+    hev_fsh_client_base_destroy ((HevFshClientBase *)client);
+}
+
+static void
+client_port_forward_run (const char *srv_addr, unsigned int srv_port,
+                         const char *tgt_addr, unsigned int tgt_port,
+                         const char *token)
+{
+    for (;;) {
+        HevFshClientPortForward *client;
+
+        client = hev_fsh_client_port_forward_new (srv_addr, srv_port, tgt_addr,
+                                                  tgt_port, token);
+
+        hev_task_system_run ();
+
+        hev_fsh_client_base_destroy ((HevFshClientBase *)client);
+
+        sleep (1);
+    }
+}
+
+static void
+client_port_listen_run (const char *lis_addr, unsigned int lis_port,
+                        const char *srv_addr, unsigned int srv_port,
+                        const char *token)
+{
+    HevFshClientPortListen *client;
+
+    client = hev_fsh_client_port_listen_new (lis_addr, lis_port, srv_addr,
+                                             srv_port, token);
 
     hev_task_system_run ();
 
@@ -89,24 +126,26 @@ int
 main (int argc, char *argv[])
 {
     int opt;
-    const char *listen_address = "0.0.0.0";
-    const char *server_address = NULL;
-    unsigned int port = 6339;
+    const char *lis_addr = "0.0.0.0";
+    const char *srv_addr = NULL;
+    const char *loc_addr = "127.0.0.1";
+    unsigned int srv_port = 6339;
+    unsigned int loc_port = 0;
+    const char *cval = NULL;
     const char *log = NULL;
     const char *user = NULL;
-    const char *login_token = NULL;
-    const char *connect_token = NULL;
+    const char *token = NULL;
 
-    while ((opt = getopt (argc, argv, "a:p:s:l:u:c:t:")) != -1) {
+    while ((opt = getopt (argc, argv, "a:p:s:l:u:c:t:o:x:")) != -1) {
         switch (opt) {
         case 'a':
-            listen_address = optarg;
+            lis_addr = optarg;
             break;
         case 's':
-            server_address = optarg;
+            srv_addr = optarg;
             break;
         case 'p':
-            port = atoi (optarg);
+            srv_port = atoi (optarg);
             break;
         case 'l':
             log = optarg;
@@ -115,10 +154,16 @@ main (int argc, char *argv[])
             user = optarg;
             break;
         case 't':
-            login_token = optarg;
+            token = optarg;
             break;
         case 'c':
-            connect_token = optarg;
+            cval = optarg;
+            break;
+        case 'o':
+            loc_addr = optarg;
+            break;
+        case 'x':
+            loc_port = atoi (optarg);
             break;
         default: /* '?' */
             show_help ();
@@ -127,7 +172,7 @@ main (int argc, char *argv[])
     }
 
     /* redirect log for server */
-    if (log && !server_address) {
+    if (log && !srv_addr) {
         int fd;
 
         fd = open (log, O_CREAT | O_APPEND | O_WRONLY, 0644);
@@ -152,13 +197,24 @@ main (int argc, char *argv[])
         return -1;
     }
 
-    if (server_address) {
-        if (connect_token)
-            client_term_connect_run (server_address, port, connect_token);
-        else
-            client_term_forward_run (server_address, port, user, login_token);
+    if (srv_addr) {
+        if (loc_port) {
+            if (cval) {
+                client_port_listen_run (loc_addr, loc_port, srv_addr, srv_port,
+                                        cval);
+            } else {
+                client_port_forward_run (srv_addr, srv_port, loc_addr, loc_port,
+                                         token);
+            }
+        } else {
+            if (cval) {
+                client_term_connect_run (srv_addr, srv_port, cval);
+            } else {
+                client_term_forward_run (srv_addr, srv_port, user, token);
+            }
+        }
     } else {
-        server_run (listen_address, port);
+        server_run (lis_addr, srv_port);
     }
 
     hev_task_system_fini ();
