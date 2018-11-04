560bf2ec1bb1a1bf1b2018196683fac3215dbaa1
Heiher <r@hev.cc>
2018-11-04 20:44:03 +0800

    HevFshClientTermAccept: Inherit from accept.

--- src/hev-fsh-client-term-accept.c
+++ src/hev-fsh-client-term-accept.c
@@ -8,42 +8,30 @@
  */
 
 #include <stdio.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
-#include <netinet/in.h>
 #include <pwd.h>
 #include <pty.h>
 
 #include "hev-fsh-client-term-accept.h"
 #include "hev-memory-allocator.h"
-#include "hev-task.h"
 #include "hev-task-io-socket.h"
-
-#define TASK_STACK_SIZE (64 * 4096)
 
 struct _HevFshClientTermAccept
 {
-    HevFshClientBase base;
-
-    HevFshToken token;
-
-    HevTask *task;
-    HevFshConfig *config;
+    HevFshClientAccept base;
 };
 
 static void hev_fsh_client_term_accept_task_entry (void *data);
-static void hev_fsh_client_term_accept_destroy (HevFshClientBase *self);
+static void hev_fsh_client_term_accept_destroy (HevFshClientAccept *base);
 
 HevFshClientBase *
 hev_fsh_client_term_accept_new (HevFshConfig *config, HevFshToken token)
 {
     HevFshClientTermAccept *self;
-    const char *address;
-    unsigned int port;
 
     self = hev_malloc0 (sizeof (HevFshClientTermAccept));
     if (!self) {
@@ -51,35 +39,22 @@ hev_fsh_client_term_accept_new (HevFshConfig *config, HevFshToken token)
         return NULL;
     }
 
-    address = hev_fsh_config_get_server_address (config);
-    port = hev_fsh_config_get_server_port (config);
-
-    if (0 > hev_fsh_client_base_construct (&self->base, address, port)) {
-        fprintf (stderr, "Construct client base failed!\n");
+    if (0 > hev_fsh_client_accept_construct (&self->base, config, token)) {
         hev_free (self);
         return NULL;
     }
 
-    self->task = hev_task_new (TASK_STACK_SIZE);
-    if (!self->task) {
-        fprintf (stderr, "Create client term's task failed!\n");
-        hev_free (self);
-        return NULL;
-    }
-
-    self->config = config;
-    memcpy (self->token, token, sizeof (HevFshToken));
     self->base._destroy = hev_fsh_client_term_accept_destroy;
 
-    hev_task_run (self->task, hev_fsh_client_term_accept_task_entry, self);
+    hev_task_run (self->base.task, hev_fsh_client_term_accept_task_entry, self);
 
-    return &self->base;
+    return &self->base.base;
 }
 
 static void
-hev_fsh_client_term_accept_destroy (HevFshClientBase *self)
+hev_fsh_client_term_accept_destroy (HevFshClientAccept *base)
 {
-    hev_free (self);
+    hev_free (base);
 }
 
 static void
@@ -87,48 +62,24 @@ hev_fsh_client_term_accept_task_entry (void *data)
 {
     HevTask *task = hev_task_self ();
     HevFshClientTermAccept *self = data;
-    HevFshMessage message;
-    HevFshMessageToken message_token;
     HevFshMessageTermInfo message_term_info;
-    int sock_fd, ptm_fd;
-    struct msghdr mh;
-    struct iovec iov[2];
+    int sfd, pfd;
     struct winsize win_size;
     ssize_t len;
     pid_t pid;
 
-    sock_fd = self->base.fd;
-    hev_task_add_fd (task, sock_fd, EPOLLIN | EPOLLOUT);
-
-    if (hev_task_io_socket_connect (sock_fd, &self->base.address,
-                                    sizeof (struct sockaddr_in), NULL,
-                                    NULL) < 0)
-        goto quit;
-
-    message.ver = 1;
-    message.cmd = HEV_FSH_CMD_ACCEPT;
-    memcpy (message_token.token, self->token, sizeof (HevFshToken));
-
-    memset (&mh, 0, sizeof (mh));
-    mh.msg_iov = iov;
-    mh.msg_iovlen = 2;
-
-    iov[0].iov_base = &message;
-    iov[0].iov_len = sizeof (message);
-    iov[1].iov_base = &message_token;
-    iov[1].iov_len = sizeof (message_token);
-
-    if (hev_task_io_socket_sendmsg (sock_fd, &mh, MSG_WAITALL, NULL, NULL) <= 0)
+    sfd = self->base.base.fd;
+    if (0 > hev_fsh_client_accept_send_accept (&self->base))
         goto quit;
 
     /* recv message term info */
-    len = hev_task_io_socket_recv (sock_fd, &message_term_info,
+    len = hev_task_io_socket_recv (sfd, &message_term_info,
                                    sizeof (message_term_info), MSG_WAITALL,
                                    NULL, NULL);
     if (len <= 0)
         goto quit;
 
-    pid = forkpty (&ptm_fd, NULL, NULL, NULL);
+    pid = forkpty (&pfd, NULL, NULL, NULL);
     if (pid == -1) {
         goto quit;
     } else if (pid == 0) {
@@ -142,7 +93,7 @@ hev_fsh_client_term_accept_task_entry (void *data)
         if (getuid () == 0) {
             const char *user;
 
-            user = hev_fsh_config_get_user (self->config);
+            user = hev_fsh_config_get_user (self->base.config);
             if (user) {
                 struct passwd *pwd;
 
@@ -169,18 +120,18 @@ hev_fsh_client_term_accept_task_entry (void *data)
         win_size.ws_xpixel = 0;
         win_size.ws_ypixel = 0;
 
-        ioctl (ptm_fd, TIOCSWINSZ, &win_size);
+        ioctl (pfd, TIOCSWINSZ, &win_size);
     }
 
-    if (fcntl (ptm_fd, F_SETFL, O_NONBLOCK) == -1)
+    if (fcntl (pfd, F_SETFL, O_NONBLOCK) == -1)
         goto quit_close_fd;
 
-    hev_task_add_fd (task, ptm_fd, EPOLLIN | EPOLLOUT);
+    hev_task_add_fd (task, pfd, EPOLLIN | EPOLLOUT);
 
-    hev_task_io_splice (sock_fd, sock_fd, ptm_fd, ptm_fd, 2048, NULL, NULL);
+    hev_task_io_splice (sfd, sfd, pfd, pfd, 2048, NULL, NULL);
 
 quit_close_fd:
-    close (ptm_fd);
+    close (pfd);
 quit:
     hev_fsh_client_base_destroy ((HevFshClientBase *)self);
 }
