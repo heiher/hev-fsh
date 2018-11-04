bcea49028cb0520f30eccb45288c696c0e793762
Heiher <r@hev.cc>
2018-11-04 20:30:06 +0800

    HevFshClientAccept: Init.

---
+++ src/hev-fsh-client-accept.c
@@ -0,0 +1,94 @@
+/*
+ ============================================================================
+ Name        : hev-fsh-client-accept.c
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Fsh client accept
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#include "hev-fsh-client-accept.h"
+#include "hev-task-io-socket.h"
+
+#define TASK_STACK_SIZE (64 * 4096)
+
+static void hev_fsh_client_accept_destroy (HevFshClientBase *base);
+
+int
+hev_fsh_client_accept_construct (HevFshClientAccept *self, HevFshConfig *config,
+                                 HevFshToken token)
+{
+    const char *address;
+    unsigned int port;
+
+    address = hev_fsh_config_get_server_address (config);
+    port = hev_fsh_config_get_server_port (config);
+
+    if (0 > hev_fsh_client_base_construct (&self->base, address, port)) {
+        fprintf (stderr, "Construct client base failed!\n");
+        return -1;
+    }
+
+    self->task = hev_task_new (TASK_STACK_SIZE);
+    if (!self->task) {
+        fprintf (stderr, "Create client accept's task failed!\n");
+        return -1;
+    }
+
+    self->config = config;
+    memcpy (self->token, token, sizeof (HevFshToken));
+    self->base._destroy = hev_fsh_client_accept_destroy;
+
+    return 0;
+}
+
+static void
+hev_fsh_client_accept_destroy (HevFshClientBase *base)
+{
+    HevFshClientAccept *self = (HevFshClientAccept *)base;
+
+    if (self->_destroy)
+        self->_destroy (self);
+}
+
+int
+hev_fsh_client_accept_send_accept (HevFshClientAccept *self)
+{
+    HevTask *task = hev_task_self ();
+    HevFshMessage message;
+    HevFshMessageToken message_token;
+    struct msghdr mh;
+    struct iovec iov[2];
+    int fd;
+
+    fd = self->base.fd;
+    hev_task_add_fd (task, fd, EPOLLIN | EPOLLOUT);
+
+    if (hev_task_io_socket_connect (fd, &self->base.address,
+                                    sizeof (struct sockaddr_in), NULL,
+                                    NULL) < 0)
+        return -1;
+
+    message.ver = 1;
+    message.cmd = HEV_FSH_CMD_ACCEPT;
+    memcpy (message_token.token, self->token, sizeof (HevFshToken));
+
+    memset (&mh, 0, sizeof (mh));
+    mh.msg_iov = iov;
+    mh.msg_iovlen = 2;
+
+    iov[0].iov_base = &message;
+    iov[0].iov_len = sizeof (message);
+    iov[1].iov_base = &message_token;
+    iov[1].iov_len = sizeof (message_token);
+
+    if (hev_task_io_socket_sendmsg (fd, &mh, MSG_WAITALL, NULL, NULL) <= 0)
+        return -1;
+
+    return 0;
+}
---
+++ src/hev-fsh-client-accept.h
@@ -0,0 +1,39 @@
+/*
+ ============================================================================
+ Name        : hev-fsh-client-accept.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Fsh client accept
+ ============================================================================
+ */
+
+#ifndef __HEV_FSH_CLIENT_ACCEPT_H__
+#define __HEV_FSH_CLIENT_ACCEPT_H__
+
+#include "hev-task.h"
+#include "hev-fsh-config.h"
+#include "hev-fsh-protocol.h"
+#include "hev-fsh-client-base.h"
+
+typedef struct _HevFshClientAccept HevFshClientAccept;
+typedef void (*HevFshClientAcceptDestroy) (HevFshClientAccept *self);
+
+struct _HevFshClientAccept
+{
+    HevFshClientBase base;
+
+    HevFshToken token;
+
+    HevTask *task;
+    HevFshConfig *config;
+
+    /* private */
+    HevFshClientAcceptDestroy _destroy;
+};
+
+int hev_fsh_client_accept_construct (HevFshClientAccept *self,
+                                     HevFshConfig *config, HevFshToken token);
+
+int hev_fsh_client_accept_send_accept (HevFshClientAccept *self);
+
+#endif /* __HEV_FSH_CLIENT_ACCEPT_H__ */
