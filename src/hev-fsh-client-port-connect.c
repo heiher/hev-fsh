ef500c1d6858f673c2b10393b4ad9e162113ed04
Heiher <r@hev.cc>
2018-11-08 21:52:43 +0800

    HevFshClientPortConnect: Fix include header files.

--- src/hev-fsh-client-port-connect.c
+++ src/hev-fsh-client-port-connect.c
@@ -18,6 +18,7 @@
 #include "hev-fsh-client-port-connect.h"
 #include "hev-fsh-protocol.h"
 #include "hev-memory-allocator.h"
+#include "hev-task-io.h"
 #include "hev-task-io-socket.h"
 
 struct _HevFshClientPortConnect
