29a15a3c13b9cc4b8cb8c17777bc5069ff9c8e88
Heiher <r@hev.cc>
2018-12-06 19:13:11 +0800

    HevMain: Bump to version 3.6.0.

--- src/hev-main.c
+++ src/hev-main.c
@@ -29,8 +29,8 @@
 #include "hev-fsh-client-port-listen.h"
 
 #define MAJOR_VERSION (3)
-#define MINOR_VERSION (5)
-#define MICRO_VERSION (6)
+#define MINOR_VERSION (6)
+#define MICRO_VERSION (0)
 
 static void
 show_help (void)
