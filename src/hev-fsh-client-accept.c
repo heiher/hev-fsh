94c7a52df978c1c044b21e34f015d946779fa989
Heiher <r@hev.cc>
2019-03-12 20:06:42 +0800

    HevFshClientAccept: Grow down task stack size.

--- src/hev-fsh-client-accept.c
+++ src/hev-fsh-client-accept.c
@@ -16,7 +16,7 @@
 #include "hev-task-io.h"
 #include "hev-task-io-socket.h"
 
-#define TASK_STACK_SIZE (64 * 4096)
+#define TASK_STACK_SIZE (8192)
 
 static void hev_fsh_client_accept_destroy (HevFshClientBase *base);
 
