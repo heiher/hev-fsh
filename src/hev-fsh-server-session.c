62d7407434e914d57c1b2c3576b4ba2f89e40a20
Heiher <r@hev.cc>
2020-03-17 01:20:22 +0800

    HevFshServerSession: Use builtin bzero.

--- src/hev-fsh-server-session.c
+++ src/hev-fsh-server-session.c
@@ -243,7 +243,7 @@ fsh_server_write_message_token (HevFshServerSession *self)
     iov[1].iov_base = &msg_token;
     iov[1].iov_len = sizeof (msg_token);
 
-    memset (&mh, 0, sizeof (mh));
+    __builtin_bzero (&mh, sizeof (mh));
     mh.msg_iov = iov;
     mh.msg_iovlen = 2;
 
@@ -297,7 +297,7 @@ fsh_server_write_message_connect (HevFshServerSession *self)
     iov[1].iov_base = &msg_token;
     iov[1].iov_len = sizeof (msg_token);
 
-    memset (&mh, 0, sizeof (mh));
+    __builtin_bzero (&mh, sizeof (mh));
     mh.msg_iov = iov;
     mh.msg_iovlen = 2;
 
