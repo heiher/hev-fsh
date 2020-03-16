27a42c261cc42e15c18f050892bed06694d11850
Heiher <r@hev.cc>
2020-03-16 14:44:09 +0800

    HevMain: Bump to version 4.2.1.

--- src/hev-main.h
+++ src/hev-main.h
@@ -14,7 +14,7 @@
 
 #define MAJOR_VERSION (4)
 #define MINOR_VERSION (2)
-#define MICRO_VERSION (0)
+#define MICRO_VERSION (1)
 
 int hev_fsh_parse_sockaddr (struct sockaddr_in6 *saddr, const char *addr,
                             int port);
